cc20ca526ddc760b38d35aaf1a5777480069a921
Andry <andrybelan@rambler.ru>
2022-05-12 22:42:47 +0300

    debag

--- New Quiz/js/app.js
+++ New Quiz/js/app.js
@@ -1,48 +1,38 @@
 
 
 
-console.log(quiz[0])
-var currentQuestionIndex= 0
+console.log(quiz[0]);
+var currentQuestionIndex= 0;
 let answers = [];
 let quizBox = document.querySelector('.Quiz-box');
 let resultBox = document.querySelector(".result-box");
-// {quizIndex, optionIndex}
 
 function onNext() {
     if (currentQuestionIndex < quiz.length) {
-        renderQuiz()
+        renderQuiz();
     } else {
-        renderResult()
+        renderResult();
     }
 }
 
 function init() {
-    currentQuestionIndex = 0
-    answers = []
-    // document.getElementById('next-2')
-    //     .addEventListener('click', function(e) {
-    //         console.log('next-2 click'. e);
-    //         onNext()
-    //     })
-    renderQuiz()
-
+    currentQuestionIndex = 0;
+    answers = [];
+    renderQuiz();
 }
 
 function renderResult() {
-    
     quizBox.classList.add("hide");
     resultBox.classList.remove("hide");
-    
+
     let quizTotal = quiz.length;
     let rightAnswers = 0;
     let wrongAnswers = 0;
-    let totalQuestion = document.querySelector(".total-question");
-    let totalCorrect = document.querySelector(".total-correct");
     let totalWrong = document.querySelector(".total-wrong");
     let percentage = document.querySelector(".percentage");
     let totalScore = document.querySelector(".total-score");
-    
-    totalQuestion.innerHTML = quizTotal;
+    let totalCorrect = document.querySelector(".total-correct");
+
     for (let i = 0; i < answers.length; i++) {
         let answer = answers[i]
         if (quiz[answer.quizIndex].answers - 1 === answer.optionIndex) {
@@ -55,8 +45,7 @@ function renderResult() {
     totalCorrect.innerHTML = rightAnswers;
     totalWrong.innerHTML = wrongAnswers;
     percentage.innerHTML = percentValue + "%";
-    totalScore.innerHTML = calculateScore(percentValue);
-   
+    totalScore.innerHTML = calculateScore(percentValue);  
 }
 
 function calculateScore(percentage) {
@@ -70,18 +59,15 @@ function calculateScore(percentage) {
     } else {
         score = 2;
     }
-
     return score;
 }
-
 function reset() {
     resultBox.classList.add("hide");
     quizBox.classList.remove("hide");
     currentQuestionIndex = 0
-    answers = []
-    renderQuiz()
+    answers = [];
+    renderQuiz();
 }
-
 function renderQuiz() {
     var optionContainer = document.querySelector('.option-container');
     var containerHead = document.querySelector('.question-text');
@@ -106,9 +92,8 @@ function renderQuiz() {
     //     var optionElm = createOption(i, option);
     //     optionContainer.appendChild(optionElm);
     // });
-    currentQuestionIndex++
+    currentQuestionIndex++;
 }
-
 function createOption(index, quizText) {
     var option = document.createElement('div');
     option.classList.add('option');
@@ -120,7 +105,6 @@ function createOption(index, quizText) {
     };
     return option;
 }
-
 function saveAnswer(optionIndex) {
     const quizIndex = currentQuestionIndex - 1;
     answers.push({
@@ -128,4 +112,3 @@ function saveAnswer(optionIndex) {
         optionIndex: optionIndex
     });
 }
-
